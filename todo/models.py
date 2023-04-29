4859a20e98c4c4a5e8f31df82cf83ab050e6b518
yuli cuadrado <andrelazaro1967@gmail.com>
2023-04-29 16:42:12 -0500

    archivos/todo actualizados

---
+++ todo/models.py
@@ -0,0 +1,31 @@
+
+from todo import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(20), unique = True, nullable = False)
+    password = db.Column(db.Text, nullable = False)
+
+    def __init__(self, username, password) -> None:
+        self.username = username
+        self.password = password
+
+    def __repr__(self) -> str:
+        return f'<User: {self.username} >'
+    
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    created_by = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
+    title = db.Column(db.String(100), nullable = False)
+    desc = db.Column(db.Text)
+    state = db.Column(db.Boolean, default = False)
+
+    def __init__(self, created_by, title, desc,  state = False ) -> None:
+        self.created_by = created_by
+        self.title = title
+        self.desc = desc
+        self.state = state
+
+    def __repr__(self) -> str:
+        return f'<Todo: {self.title} >'
---
+++ todo/todo.py
@@ -0,0 +1,80 @@
+
+
+                                   # administración de Vistas
+
+
+from flask import Blueprint, render_template, redirect, request, url_for, g
+
+from todo.auth import login_required
+
+from .models import Todo, User
+from todo import db
+
+bp = Blueprint('todo', __name__, url_prefix='/todo')
+
+
+
+# para listar tareas
+
+@bp.route('/list')   # ruta
+@login_required     # decorador - indica q se requiere iniciar sesión
+def index():       # vista
+    todos = Todo.query.all()  # recupera todo
+    return render_template('todo/index.html', todos = todos)
+
+
+# para crear tareas
+
+@bp.route('/create', methods=('GET', 'POST'))
+@login_required     # decorador - indica q se requiere iniciar sesión
+def create():
+    if request.method == 'POST':
+        title = request.form['title'] # datos del formulario capturados desde la base de datos
+        desc = request.form['desc']   # datos del formulario capturados desde la base de datos
+
+        todo = Todo(g.user.id, title, desc) # manera q se crea una tarea - se captura el id de la persona q inició sesión
+        
+        # guardar en la base de datos:
+        db.session.add(todo)    # añade
+        db.session.commit()     # 
+        return redirect(url_for('todo.index'))
+    return render_template('todo/create.html')
+
+
+
+# para editar las tareas
+
+def get_todo(id):     # función para retornar una TAREA mediante un id
+    todo = Todo.query.get_or_404(id) # busca la tarea; si no encuentra en lista de tarea devuelve un error 404
+    return todo # devuelve la tarea q se está buscando
+
+
+# se crea la Vista de editar
+
+@bp.route('/update/<int:id>', methods=('GET', 'POST'))
+@login_required
+def update(id):  
+      
+    todo = get_todo(id) # utiliza la función (get_todo) para buscar en la base de datos la tarea con el id
+
+    if request.method == 'POST':
+        todo.title = request.form['title'] # modifica los datos
+        todo.desc = request.form['desc']   # modifica los datos
+        todo.state = True if request.form.get('state') == 'on' else False
+
+        # guardar en la base de datos:
+        db.session.commit()     # se realiza los cambios en la base de datos
+        return redirect(url_for('todo.index'))
+    return render_template('todo/update.html', todo = todo)
+
+
+# para eliminar una tarea
+
+@bp.route('/delete/<int:id>')  # se requiere recibir un id
+@login_required
+def delete(id):  # función para eliminar la tarea
+    todo = get_todo(id)     # busca el id ingresado
+    db.session.delete(todo) # se envia el id de la tarea recibido para borrarla
+    db.session.commit()     # se envia modificación a la base de datos
+    return redirect(url_for('todo.index'))
+      
